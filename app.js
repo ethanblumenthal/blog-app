e7e715692667ba2468786244a30a4e531c099d93
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-14 11:21:02 -0800

    basic setup

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/blog_app');
+mongoose.connect('mongodb://localhost/blog_app', {useMongoClient: true});
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, function() {
+    console.log('Server is running!');
+});
